223e814b915203d632ea7902d708e1df01d83bf6
Luis Fernando Bezerra da Silva <luisfernandobezerradasilva@hotmail.com>
2023-10-27 02:04:08 -0300

    retry

--- user/src/main/java/com/example/user/repository/UserRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.user.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.example.user.entity.User;
-
-public interface UserRepository extends JpaRepository<User, Long> {
-    public User findById(Long id);
-}
--- user/src/test/java/com/example/user/UserApplicationTests.java
+++ user/src/test/java/com/example/user/UserApplicationTests.java
@@ -1,5 +1,8 @@
 package com.example.user;
 
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
 @SpringBootTest
 class UserApplicationTests {
 
